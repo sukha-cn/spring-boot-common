91b3971e7e0b9636779045a1df7910aa4b5df537
sukha-cn <1-lion@163.com>
2018-04-30 10:23:37 +0800

    test webhook

--- src/main/java/com/local/spc/spcbase/web/TestController.java
+++ src/main/java/com/local/spc/spcbase/web/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping("/")
     public String index() {
-        return "Hello SpringBoot.";
+        return "Hello SpringBoot, test webhook";
     }
 }
